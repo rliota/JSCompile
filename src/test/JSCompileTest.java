3237e3d9e43f91e67059621821240256968ee350
rliota <robertliota.public@gmail.com>
2015-03-05 23:57:40 -0500

    CompileJS-master: Fixed issues with init function name generation if target file contains dashes.

--- src/test/JSCompileTest.java
+++ src/test/JSCompileTest.java
@@ -8,12 +8,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class JSCompileTest {
 
     public String COMPILE_SRC = "";
-    public String COMPILE_TARGET = "./test-resources/mockCompileTargetDirectory/mockproject.js";
+    public String COMPILE_TARGET = "./test-resources/mockCompileTargetDirectory/";
 
     @Before
     public void setUp() throws Exception {
@@ -22,18 +25,34 @@ public class JSCompileTest {
 
     @After
     public void tearDown() throws Exception {
-        File target = new File(COMPILE_TARGET);
+        File target = new File(COMPILE_TARGET+"mockproject.js");
+        File target2 = new File(COMPILE_TARGET+"I-have-dashes-yo.js");
         if(target.exists()){
             target.delete();
+        }
+        if(target2.exists()){
+            target2.delete();
         }
     }
 
 
     @Test
     public void testProjectCompilationFromNonLocalDirectory() throws CircularDependencyException, UnknownImportException, IOException {
-        String[] args = {"./test-resources/mockproject", COMPILE_TARGET};
+        String[] args = {"./test-resources/mockproject", COMPILE_TARGET+"mockproject.js"};
         JSCompile.main(args);
-        File expectedFile = new File(COMPILE_TARGET);
+        File expectedFile = new File(COMPILE_TARGET+"mockproject.js");
         assert(expectedFile.exists());
     }
+
+
+    @Test
+    public void testPackageNameOutputDoesNotAffectPackageFunctionName() throws CircularDependencyException, UnknownImportException, IOException {
+        String[] args = {"./test-resources/mockproject", COMPILE_TARGET+"I-have-dashes-yo.js"};
+        JSCompile.main(args);
+        File expectedFile = new File(COMPILE_TARGET+"I-have-dashes-yo.js");
+        assert(expectedFile.exists());
+        Scanner s = new Scanner(expectedFile);
+        assert(s.findWithinHorizon(Pattern.quote("function mockproject(){"), 200) != null);
+    }
+
 }
